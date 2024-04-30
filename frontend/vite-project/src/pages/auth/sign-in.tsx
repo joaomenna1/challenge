e270416032e075af0ed8cd2b12443599a02e536e
joaomenna1 <joaomenna1@gmail.com>
2024-04-30 02:19:13 -0400

    page signIn

---
+++ frontend/vite-project/src/pages/auth/sign-in.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button'
+import { Helmet } from 'react-helmet-async'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+export function SignIn() {
+    return (
+        <>
+            <Helmet title="Login"/>
+            <div className="p-8">
+              <div className="flex w-[350px] flex-col justify-center gap-6">
+                <div className="flex flex-col gap-2 text-center">
+                    <h1 className="text-2xl font-semibold tracking-tighter">
+                        Acessar painel
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        Acompanhe suas websites pela plataforma de rastreamento
+                    </p>
+                </div>
+
+                <form className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Seu e-mail</Label>
+                        <Input id="email" type="email"/>
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="password">Senha</Label>
+                        <Input id="password" type="string"/>
+                    </div>
+
+                    <Button className="w-full" type="submit">Acessar Painel</Button>
+
+                </form>
+              </div>
+            </div>
+        </>
+    )
+}
